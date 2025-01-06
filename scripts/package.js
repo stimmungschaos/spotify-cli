8056e274c493aa13c687f2541f0d0d651283e372
root <root@vs-15295>
2025-01-06 15:39:57 +0100

    catSigh

--- scripts/package.js
+++ scripts/package.js
@@ -4,6 +4,11 @@ import path from 'path';
 
 if (process.platform === 'win32') {
   // Windows-spezifische Befehle
+  execSync('if not exist bin\\spotify-cli-win mkdir bin\\spotify-cli-win');
+  
+  fs.copyFileSync('dist/index.js', 'bin/spotify-cli-win/index.js');
+  fs.writeFileSync('bin/spotify-cli-win/spotify-cli.cmd', '@node "%~dp0index.js" %*');
+  
   fs.writeFileSync('bin/spotify-cli-win/install.bat', `
 @echo off
 set INSTALL_DIR=%LOCALAPPDATA%\\spotify-cli
@@ -14,9 +19,7 @@ setx PATH "%PATH%;%INSTALL_DIR%"
 echo Installation abgeschlossen! Bitte starten Sie Ihre Kommandozeile neu.
 pause
   `.trim());
-  execSync('if not exist bin\\spotify-cli-win mkdir bin\\spotify-cli-win');
-  fs.copyFileSync('dist/index.js', 'bin/spotify-cli-win/index.js');
-  fs.writeFileSync('bin/spotify-cli-win/spotify-cli.cmd', '@node "%~dp0index.js" %*');
+  
   execSync('powershell Compress-Archive -Force -Path ".\\bin\\spotify-cli-win\\*" -DestinationPath ".\\spotify-cli-win.zip"');
 } else {
   // Linux Build
